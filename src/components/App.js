62d6ea639ffcf16ec50400f0b418ccce5f608e10
RITESH ROUSHAN KUMAR <78962046+Ritesh1408@users.noreply.github.com>
2025-04-03 18:03:47 +0000

    Completed the Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,23 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
+import ChildComponent1 from "./ChildComponent1";
+import ChildComponent2 from "./ChildComponent2";
 
 const App = () => {
+  const [selectedOption, setSelectedOption] = useState("");
+
+  const handleSelection = (option) => {
+    setSelectedOption(option);
+  };
+
   return (
-    <div>
+    <div className="parent" style={{backgroundColor: 'lightgreen', padding: '15px', width: '50%'}}>
         {/* Do not remove the main div */}
+        <h1>Parent Component</h1>
+      <ChildComponent1 onSelect={handleSelection} />
+      <ChildComponent2 onSelect={handleSelection} />
+      <p>Selected Option: {selectedOption || ""}</p>
     </div>
   )
 }
